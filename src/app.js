72a7a199a9c8fbdb67c7b992e1f84056da4bdae4
Revan Sopher <rsopher@gmail.com>
2015-08-27 02:22:09 -0400

    Option to get nearest bus stop.

--- src/app.js
+++ src/app.js
@@ -35,6 +35,21 @@ var parseStops = function(data) {
 	return items;
 };
 
+//parses /stop/$STOP
+var parseRoutesForStop = function(data) {
+	var items = [];
+	data.forEach(function(route) {
+		if (route.predictions !== null) {
+			items.push({
+				title:route['title'],
+				predictions:route['predictions']
+			});
+		}
+	});
+	
+	return items;
+};
+
 // Gets menu items for location screen
 var parseLocations = function(data) {
 	var items = [];
@@ -45,6 +60,29 @@ var parseLocations = function(data) {
 	});
 	
 	return items;
+};
+
+var getNearestStop = function(data, lat, lon) {
+	console.log("GEOLOCATION: " + lat + ", " + lon);
+	var closestStop = null;
+	var minDistance = Number.MAX_VALUE;
+	
+	for (var stop in data.stops) {
+		if (data.stops.hasOwnProperty(stop)) {
+			var latDiff = lat - parseFloat(data.stops[stop].lat);
+			var lonDiff = lon - parseFloat(data.stops[stop].lon);
+			var distSq = latDiff * latDiff + lonDiff * lonDiff;
+			if (distSq < minDistance) {
+				minDistance = distSq;
+				closestStop = stop;
+			}
+		}
+	}
+	
+	data.stops[closestStop].tag = closestStop;
+	
+	console.log("CLOSEST STOP: " + closestStop);
+	return data.stops[closestStop];
 };
 
 var parseMeals = function(data, location) {
@@ -215,45 +253,107 @@ mainMenu.on('select', function(e) {
 			var routeMenu = new UI.Menu({
 				highlightBackgroundColor: 'red',
 				sections: [{
+					title: '',
+					items: [{title:'Nearest'}]
+				},
+				{
 					title: 'Routes',
 					items: routeItems
 				}]
 			});
 			routeMenu.on('select', function(e) {
-				var tag = data['active']['routes'][e.itemIndex]['tag'];
-				ajax(
-					{
-						url:'http://runextbus.herokuapp.com/route/'+tag,
-						type:'json'
-					},
-					function(data) {
-						var stops = data;
-						var stopItems = parseStops(data)
-						var stopMenu = new UI.Menu({
-							highlightBackgroundColor: 'red',
-							sections: [{
-								title: 'Stops: ' + tag,
-								items: stopItems
-							}]
-						});
-						stopMenu.on('select', function(e) {
-							var stop = stops[e.itemIndex];
-							var body = "";
-							stop['predictions'].forEach(function(pred) {
-								body += pred['minutes'] + 'min\n';
+				if (e.sectionIndex === 0) {
+					// get nearest
+					splashWindow.show();
+					navigator.geolocation.getCurrentPosition(
+						function(pos) {
+							var stop = getNearestStop(data, pos.coords.latitude, pos.coords.longitude);
+							// have nearest, now get and show all its routes
+							ajax(
+								{
+									url:'http://runextbus.herokuapp.com/stop/'+stop.tag,
+									type:'json'
+								},
+								function(data) {
+									splashWindow.hide();
+									
+									var routes = parseRoutesForStop(data);
+
+									var routeForStopMenu = new UI.Menu({
+										highlightBackgroundColor: 'red',
+										sections: [{
+											title: stop.title,
+											items: routes
+										}]
+									});
+									
+									routeForStopMenu.on('select', function(e) {
+										var route = routes[e.itemIndex];
+										var body = "";
+										route.predictions.forEach(function(pred) {
+											body += pred.minutes + 'min\n';
+										});
+										var stopCard = new UI.Card({
+											title:stop.title + ": " + route.title,
+											body:body
+										});
+										stopCard.show();
+									});
+									
+									routeForStopMenu.show();
+								},
+								function(error) {
+									console.log("Download failed: " + error);
+									splashWindow.hide();
+								}
+							);
+						},
+						function(err) {
+							splashWindow.hide();
+						},
+						{
+							enableHighAccuracy: true, 
+							maximumAge: 10000, 
+							timeout: 10000
+						}
+					);
+				} else {
+					// picking a route
+					var tag = data['active']['routes'][e.itemIndex]['tag'];
+					ajax(
+						{
+							url:'http://runextbus.herokuapp.com/route/'+tag,
+							type:'json'
+						},
+						function(data) {
+							var stops = data;
+							var stopItems = parseStops(data)
+							var stopMenu = new UI.Menu({
+								highlightBackgroundColor: 'red',
+								sections: [{
+									title: 'Stops: ' + tag,
+									items: stopItems
+								}]
 							});
-							var stopCard = new UI.Card({
-								title:stop['title'] + ": " + tag,
-								body:body
+							stopMenu.on('select', function(e) {
+								var stop = stops[e.itemIndex];
+								var body = "";
+								stop['predictions'].forEach(function(pred) {
+									body += pred['minutes'] + 'min\n';
+								});
+								var stopCard = new UI.Card({
+									title:stop['title'] + ": " + tag,
+									body:body
+								});
+								stopCard.show();
 							});
-							stopCard.show();
-						});
-						stopMenu.show();
-					},
-					function(error) {
-						console.log("Download failed: " + error);
-					}
-				);
+							stopMenu.show();
+						},
+						function(error) {
+							console.log("Download failed: " + error);
+						}
+					);
+				}
 			});
 			
 			// Show the Menu, hide the splash
